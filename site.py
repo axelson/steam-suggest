e7f41c7b298a3e1d4fc632684388a995ce7c342f
Jason Axelson <jason.axelson@gmail.com>
2011-03-16 00:24:36 -0700

    cgi handling

--- site.py
+++ site.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import re
+import cgi
 
 print "Content-type: text/html\n\n"
 print "Hello, you own the following games:"
@@ -10,5 +11,24 @@ f = open('mypage.html')
 games = re.findall("rgGames\[.*= '(.*)';", f.read())
 
 for game in games :
-    print game
-    print "<br>"
+    a = 1
+    #print game
+    #print "<br>"
+
+form = cgi.FieldStorage()
+message = form.getvalue("message", "(no message)")
+
+print """
+
+<p>Previous message: %s</p>
+
+<p>form
+
+<form method="post" action="site.py">
+<p>message: <input type="text" name="message"/></p>
+</form>
+
+</body>
+
+</html>
+""" % cgi.escape(message)
