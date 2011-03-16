ad7f4a9edcbdad17c89a7771a7bf2b14fb05796f
Jason Axelson <jason.axelson@gmail.com>
2011-03-16 00:07:57 -0700

    Read in the names of the games

--- site.py
+++ site.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python
+import re
+
 print "Content-type: text/html\n\n"
 print "Hello world"
+
+print "<br><br>"
+
+f = open('mypage.html')
+m = re.findall("rgGames\[.*= '(.*)';", f.read())
+
+print m
