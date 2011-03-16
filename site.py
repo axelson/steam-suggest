77258f592e490fc78b54f6f2addce312ea993376
Jason Axelson <jason.axelson@gmail.com>
2011-03-16 10:39:07 -0700

    Read in steamid and download page from steam

--- site.py
+++ site.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python
 import re
 import cgi
+import urllib
 
 print "Content-type: text/html\n\n"
 print "<br><br>"
 
-f = open('mypage.html')
-games = re.findall("rgGames\[.*= '(.*)';", f.read())
-
 form = cgi.FieldStorage()
-message = form.getvalue("message", "(no message)")
+steamid = form.getvalue("steamid", "defaultvalue")
+filter = form.getvalue("filter", "(no message)")
+
+if steamid == "defaultvalue":
+    f = open('mypage.html')
+    text = f.read()
+else:
+    text = urllib.urlopen('http://steamcommunity.com/profiles/76561197973312035/games?tab=all').read()
+
+games = re.findall("rgGames\[.*= '(.*)';", text)
 
 print """
 
@@ -18,18 +25,20 @@ print """
 <p>form
 
 <form method="post" action="site.py">
-<p>message: <input type="text" name="message"/></p>
+<p>steamid: <input type="text" name="steamid"/></p>
+<p>filter: <input type="text" name="filter"/></p>
+<input type="submit" />
 </form>
 
 </body>
 
 </html>
-""" % cgi.escape(message)
+""" % cgi.escape(steamid)
 
-print "Hello, of the games you own, the following games match (" + message + "):"
+print "Hello, of the games you own, the following games match (" + filter + "):"
 print "<br>"
 
 for game in games :
-    if re.search(message, game) :
+    if re.search(filter, game) :
         print game
         print "<br>"
