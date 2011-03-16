28c1fe7eb3ef0218475d1c1afe440fccf03bcecd
Jason Axelson <jason.axelson@gmail.com>
2011-03-16 00:32:10 -0700

    Print games matching the form

--- site.py
+++ site.py
@@ -3,17 +3,10 @@ import re
 import cgi
 
 print "Content-type: text/html\n\n"
-print "Hello, you own the following games:"
-
 print "<br><br>"
 
 f = open('mypage.html')
 games = re.findall("rgGames\[.*= '(.*)';", f.read())
-
-for game in games :
-    a = 1
-    #print game
-    #print "<br>"
 
 form = cgi.FieldStorage()
 message = form.getvalue("message", "(no message)")
@@ -32,3 +25,11 @@ print """
 
 </html>
 """ % cgi.escape(message)
+
+print "Hello, of the games you own, the following games match (" + message + "):"
+print "<br>"
+
+for game in games :
+    if re.search(message, game) :
+        print game
+        print "<br>"
