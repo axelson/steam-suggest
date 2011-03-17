ddfa46d30f0580a1c743a2e6650779d3174ddaec
Jason Axelson <jason.axelson@gmail.com>
2011-03-16 23:15:19 -0700

    Dynamically fetch the correct steam page

--- site.py
+++ site.py
@@ -6,21 +6,35 @@ import urllib
 print "Content-type: text/html\n\n"
 print "<br><br>"
 
+# Profiles
+bostonvaulter = "76561197973312035"
+jibbers = "76561197977685020"
+positron = "positrons"
+
+defaultSteamId = "defaultvalue"
+
 form = cgi.FieldStorage()
-steamid = form.getvalue("steamid", "defaultvalue")
+steamid = form.getvalue("steamid", defaultSteamId)
 filter = form.getvalue("filter", "(no message)")
 
-if steamid == "defaultvalue":
+url = "default"
+if steamid == defaultSteamId:
     f = open('mypage.html')
     text = f.read()
 else:
-    text = urllib.urlopen('http://steamcommunity.com/profiles/76561197973312035/games?tab=all').read()
+    # Need to use /profiles for numerical ids
+    # Need to use /id for custon id's
+    url = 'http://steamcommunity.com/profiles/' + steamid + '/games?tab=all'
+    text = urllib.urlopen(url).read()
 
 games = re.findall("rgGames\[.*= '(.*)';", text)
 
+#if they set up their own id then use /id if they're using a profile number than use /profiles
+
+print "Checking url: " + url
 print """
 
-<p>Previous message: %s</p>
+<p>Checking steamid: %s</p>
 
 <p>form
 
@@ -35,7 +49,7 @@ print """
 </html>
 """ % cgi.escape(steamid)
 
-print "Hello, of the games you own, the following games match (" + filter + "):"
+print "Of the games you own, the following games match (" + filter + "):"
 print "<br>"
 
 for game in games :
